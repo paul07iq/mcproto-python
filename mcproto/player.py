4f80ab417c618f239e53a28c6c00f5526cd2d10f
paul07iq <127239640+paul07iq@users.noreply.github.com>
2025-08-19 11:07:24 +0200

    Update player.py
    - Duration Standartwert auf 5s erhöht

--- mcproto/player.py
+++ mcproto/player.py
@@ -119,9 +119,9 @@ class Player(Entity, HasStub):
     def deop(self) -> None:
         HasStub.runCommand(self, f"deop {self.name}")
 
-    def showTitle(self, text: str, typ: Literal["actionbar", "subtitle", "title"] = "title", color: COLOR = "gray", bold: bool = "false", italic: bool = "false", strikethrough: bool = "false", underlined: bool = "false", obfuscated: bool = "false", duration: int = 3, fade_in: int = 1, fade_out: int = 1) -> None:
-        HasStub.runCommand(self, f'title {self.name} times {fade_in}s {duration}s {fade_out}s')
-        HasStub.runCommand(self, f'title {self.name} {typ} ' + '{' + f'"text":"{text}","color":"{color}","bold":{bold},"italic":{italic},"strikethrough":{strikethrough},"underlined":{underlined},"obfuscated":{obfuscated}' + '}')
+    def showTitle(self, text: str, typ: Literal["actionbar", "subtitle", "title"] = "title", color: COLOR = "gray", bold: bool = False, italic: bool = False, strikethrough: bool = False, underlined: bool = False, obfuscated: bool = False, duration: int = 5, fade_in: int = 1, fade_out: int = 1) -> None:
+        self.runCommand(self, f'title {self.name} times {fade_in}s {duration}s {fade_out}s')
+        self.runCommand(self, f'title {self.name} {typ} ' + '{' + f'"text":"{text}","color":"{color}","bold":{bold},"italic":{italic},"strikethrough":{strikethrough},"underlined":{underlined},"obfuscated":{obfuscated}' + '}')
 
     def clearTitle(self):
         self.runCommand("title @s clear")
